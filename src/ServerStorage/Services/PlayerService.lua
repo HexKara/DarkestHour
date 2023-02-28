49a809b1a79f1134d8b41d726977be9791acd66a
HexKara <104640630+HexKara@users.noreply.github.com>
2023-02-27 18:59:23 -0600

    Leaderstats done

--- src/ServerStorage/Services/PlayerService.lua
+++ src/ServerStorage/Services/PlayerService.lua
@@ -3,7 +3,7 @@ local playerSerives = {}
 local datastoreService = game:GetService("DataStoreService")
 local datastore = datastoreService:GetDataStore("PlayerData")
 
-local function SetLeaderstats(player)
+local function SetLeaderstats(player : Player)
     local data = datastore:GetAsync(player.UserId)
     if data then
         for i,v in pairs(data) do
@@ -12,20 +12,28 @@ local function SetLeaderstats(player)
     end
 end
 
-local function AddLeaderstats(player)
+local function AddLeaderstats(player : Player)
     local leaderstats = Instance.new("Folder")
     leaderstats.Name = "leaderstats"
     leaderstats.Parent = player
-    SetLeaderstats(player)
+    SetLeaderstats(player) 
 end
 
-local function GetLeaderstats(player)
-    
+local function SaveLeaderstats(player : Player)
+    local data = {}
+    for i,v in pairs(player.leaderstats:GetChildren()) do
+        data[v.Name] = v.Value
+    end
+    datastore:SetAsync(player.UserId, data)
 end
 
 game.Players.PlayerAdded:Connect(function(player)
     AddLeaderstats(player)
     
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+    SaveLeaderstats(player)
 end)
 
 return playerSerives
